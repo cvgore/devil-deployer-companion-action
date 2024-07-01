cf6fedd6232070e352ed159013ac8233a9720e17
cvgore <hi@cvgo.re>
2024-07-01 23:09:17 +0200

    implement devil deployer companion action

--- src/main.js
+++ src/main.js
@@ -1,28 +1,84 @@
 const core = require('@actions/core')
-const { wait } = require('./wait')
+const http = require('@actions/http-client')
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
-async function run() {
-  try {
-    const ms = core.getInput('milliseconds', { required: true })
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+async function tailDeployStatus(client, url, appName, secretKey, deploymentId) {
+  let status = null
+  let cursor = 0
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+  while (status === null) {
+    url.searchParams.set('action', 'deploymentStatus')
+    const response = await client.postJson(url.toString(), {
+      appName,
+      secretKey,
+      deploymentId,
+      cursor
+    })
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    core.setFailed(error.message)
+    const lines = response.result.split('\n')
+
+    for (const line of lines) {
+      const [nextCursor, rawEntry] = JSON.parse(line)
+      const entry = JSON.parse(rawEntry)
+
+      if (['err', 'omg'].includes(entry.type)) {
+        core.error(entry.msg)
+      } else if (entry.type === 'inf') {
+        core.info(entry.msg)
+      } else if (entry.type === 'DED') {
+        core.error(entry.msg)
+        core.setFailed(entry.msg)
+        status = 'fail'
+        break
+      } else if (entry.type === 'OKI') {
+        core.info(entry.msg)
+        status = 'ok'
+        break
+      }
+
+      cursor = nextCursor
+    }
+
+    await sleep(1000)
   }
+}
+
+async function run() {
+  const secretKey = core.getInput('secretKey')
+  const appName = core.getInput('appName')
+  const baseUrl = core.getInput('url')
+  const url = new URL(baseUrl)
+  url.searchParams.set('action', 'deploy')
+
+  const client = new http.HttpClient()
+
+  core.debug(`connecting to ${baseUrl}`)
+  const response = await client.postJson(url.toString(), {
+    appName,
+    secretKey
+  })
+  core.debug(
+    `deployment id is ${response.result.deploymentId}, starting to tail the deployment status`
+  )
+  await tailDeployStatus(
+    client,
+    url,
+    appName,
+    secretKey,
+    response.result.deploymentId
+  )
+  core.debug(`deployment id is ${response.result.deploymentId}, done`)
+  url.searchParams.set('action', 'clearDeployment')
+  core.debug(`deployment id ${response.result.deploymentId} will be cleared`)
+  await client.postJson(url.toString(), {
+    appName,
+    secretKey,
+    deploymentId: response.result.deploymentId
+  })
+  core.debug(`deployment id ${response.result.deploymentId} cleared`)
 }
 
 module.exports = {
